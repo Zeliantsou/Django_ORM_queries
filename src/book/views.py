1e892dbc81930bef3db0017466ef4d4e61dd05b6
evgeny <exxp89@gmail.com>
2022-02-16 18:09:49 +0300

    Settings, models, migrations
    1. Update settings
    2. Make migrations
    3. add Dockerfile, docker-compose

--- src/book/views.py
+++ src/book/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.decorators import action
+from rest_framework.mixins import (
+    CreateModelMixin,
+    RetrieveModelMixin,
+    UpdateModelMixin,
+    DestroyModelMixin,
+    ListModelMixin,
+)
 
-# Create your views here.
+from book.models import Author, Publisher, Book, Sales
+
+
+class AuthorViewSet(CreateModelMixin,
+                    RetrieveModelMixin,
+                    UpdateModelMixin,
+                    DestroyModelMixin,
+                    ListModelMixin,
+                    GenericViewSet):
+    """View set for Author model"""
+    queryset = Author.objects.all()
+
+
+class PublisherViewSet(CreateModelMixin,
+                    RetrieveModelMixin,
+                    UpdateModelMixin,
+                    DestroyModelMixin,
+                    ListModelMixin,
+                    GenericViewSet):
+    """View set for Publisher model"""
+    queryset = Publisher.objects.all()
+
+
+class BookViewSet(CreateModelMixin,
+                    RetrieveModelMixin,
+                    UpdateModelMixin,
+                    DestroyModelMixin,
+                    ListModelMixin,
+                    GenericViewSet):
+    """View set for Book model"""
+    queryset = Book.objects.all()
+
+
+class SalesViewSet(CreateModelMixin,
+                    RetrieveModelMixin,
+                    UpdateModelMixin,
+                    DestroyModelMixin,
+                    ListModelMixin,
+                    GenericViewSet):
+    """View set for Sales model"""
+    queryset = Sales.objects.all()
